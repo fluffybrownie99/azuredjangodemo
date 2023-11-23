777e650aff24f91493d35a014a820e9e208c4a6f
omgitsjohn <syin18@my.bcit.ca>
2023-11-22 18:12:04 -0800

    extracted example output into a txt file

--- questionGenerator.py
+++ questionGenerator.py
@@ -4,11 +4,16 @@ import os
 model = {3:"gpt-3.5-turbo", 4:"gpt-4-1106-preview"}
 load_dotenv()
 client = openai.OpenAI(api_key=os.getenv("OPENAI_KEY"))
+
+def get_prompt(file="prompt.txt"):
+    with open(file, "r") as f:
+        return f.read().replace("\n", "")
+    
 def generate_questions(user_prompt):
     messages =[{"role": "system","content": """Your response must be a list of dictionary key-value pairing related to the users input. The length of the list of dictionaries is determined by the user. The correct answer is the dictonary key's value.
     If no input is given, then makes 3 questions by default."""},
     {"role": "user","content": "ww1 2 questions"},
-    {"role":"assistant", "content":"""[{":"Question": When did the second world war begin, "Options": ["1914", "1944", "1945", "1939"], "Answer": "1939"}, {"Question": ""Who was the leader of Nazi Germany?","Options": ["Jospeh Stalin", "Joseph Goebbels", "Heinrich Himmler", "Adolf Hitler"], "Answer": "Adolf Hitler"}]"""},
+    {"role":"assistant", "content":get_prompt()},
     {"role": "user","content": f"{user_prompt}"},]
     return client.chat.completions.create(
         model=model[3],
